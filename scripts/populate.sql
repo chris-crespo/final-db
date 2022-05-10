51639055e8f656a42ba5df1dd7eb29435ab7ea0e
krs98 <christian.crespoo.98@gmail.com>
2022-05-10 10:45:49 +0000

    switch for sha hashes

--- scripts/populate.sql
+++ scripts/populate.sql
@@ -34,11 +34,11 @@ insert into camp_lang values (6, 'Inglés');
 insert into camp_lang values (7, 'Español');
 insert into camp_lang values (7, 'Chino');
 
-insert into app_user values ('1daw3',     '1daw3@plaiaundi.net',     '462C8110A356E43E3CE6FA7E5780D087', 'unode', 'dautres', '600000000');
-insert into app_user values ('cartwheel', 'cartwheel@cartwheel.com', '85418522C54769979BD5AFC5018BAA03', 'cart',  'wheel',   '600030000');
-insert into app_user values ('soybeans',  'soybeans@soybeans.com',   '8EF95EE9C8CDA8FE4153337519BA5ADE', 'soy',   'beans',   '612312858');
-insert into app_user values ('croissant', 'croissant@croissant.com', '1D3CF196DB919AA9A1BBDFB5310590E4', 'croi',  'sant',    '602123942');
-insert into app_user values ('cowwebbee', 'cowwebbee@cowwebbee.com', '37A079CA5B576ACD313B5E3EB54FA2B2', 'cow',   'webbee',  '622322983');
+insert into app_user values ('1daw3',     '1daw3@plaiaundi.net',     '$2a$12$qfxCgEdciLOeFdPZlLMcru3fqHkgwwtxAOxcKjCiZiKkNiq0zLq.O', 'unode', 'dautres', '600000000');
+insert into app_user values ('cartwheel', 'cartwheel@cartwheel.com', '$2a$12$kbf1NPIFGMK7Faxt2/SdXur8VzEl6FEC8JIyVrMcx1/r1hqjWY1NO', 'cart',  'wheel',   '600030000');
+insert into app_user values ('soybeans',  'soybeans@soybeans.com',   '$2a$12$OFCYH1Er1n3UnVdQjPb9KeOtzLu3rhgMg/fI0ahlDuc0x7j.bibNm', 'soy',   'beans',   '612312858');
+insert into app_user values ('croissant', 'croissant@croissant.com', '$2a$12$AZR0dKCCPt8VXsDaiOAY5.CVaeSnUHWML7ARvrvp0E1vGVZh7/ezq', 'croi',  'sant',    '602123942');
+insert into app_user values ('cowwebbee', 'cowwebbee@cowwebbee.com', '$2a$12$jQJB/ucaqPo6c5kOVZ/QLORvqcJtHDg58b7JHaxA.aXQBoKo2kRtq', 'cow',   'webbee',  '622322983');
 
 insert into kid values ('12213203A', 'Celyn',        'Stephenson', 10, '1daw3@plaiaundi.net');
 insert into kid values ('12313131A', 'Randall',      'Jacobs',     8,  '1daw3@plaiaundi.net');
